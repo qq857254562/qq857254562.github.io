23ee8da8161c238ee5f896c99fd5800cf05ee5e7
zhaofeiyang <857254562@qq.com>
2020-03-10 22:50:26 +0800

    react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>hello word,妞妞</span>
+        <p>react</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
