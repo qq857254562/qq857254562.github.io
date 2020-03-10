29058ff884d9f6b76191b9b473f2d0b50c9c37b5
zhaofeiyang <857254562@qq.com>
2020-03-10 23:21:37 +0800

    build

---
+++ build/precache-manifest.68b358e9e745eed2a1b77b4e62d16317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5169fbdbe4373c81cb5260c58ca6ccac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5647e39eebd85e3cc1b3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "befbebbf29c116c06854",
+    "url": "/static/js/2.21519a53.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.21519a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5647e39eebd85e3cc1b3",
+    "url": "/static/js/main.df58112a.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
